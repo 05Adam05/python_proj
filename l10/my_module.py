69a323007f0ea257c4e2a5516878e8283a43bac9
05Adam05 <32506935+05Adam05@users.noreply.github.com>
2017-10-18 11:26:36 +0300

    Add files via upload

---
+++ l10/my_module.py
@@ -0,0 +1,14 @@
+import random
+from math import sqrt, cos
+from cmath import sqrt as csqrt
+import my_factorial
+
+
+# my_list = ["1C", "Google", "Telegram", "Yandex", "Rambler", "ТЫ"]
+# print(random.choice(my_list), "уничтожен!")
+
+# print(sqrt(4))
+# print(cos(0.5))
+# print(csqrt(-1))
+
+print(my_factorial.lavash_factorial(int(input("Введите число"))))
