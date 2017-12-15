d11ba45b9880c1e11d74e3d1a5831f0b9875a40c
05Adam05 <32506935+05Adam05@users.noreply.github.com>
2017-12-15 11:30:03 +0300

    Add files via upload

---
+++ h7/java.js
@@ -0,0 +1,57 @@
+// str1 = 'Hello, World'
+// str2 = 'My name is Adam'
+// document.write(str1 + '<br>' + str2)
+// // document.write("<a href = 'http://yandex.ru'>Яндекс</div>
+
+// var day = new Date();
+// var today = day.getDay();
+
+// switch(today)
+// {
+// 	case 1;
+// 	alert("Понедельник! У тя курсы ");
+
+
+// 	case 5;
+// 	alert("Сегодня пятница")
+// }
+
+
+// // var a = prompt("Введи число");yyyyyyyyyyyyyyy
+// var f = 1
+
+// for (var i = 1; i<=a; i++)
+{
+// 	f = f * i;
+// }
+// // alert(f)
+
+// var a = 1;
+// var b = 1;
+
+
+// document.write(++a + 5);
+// document.write("<br>")
+// document.write(5 + b++)
+// var a = 3;
+
+// whil (a > 0)
+// {
+// 	document.write("<p>Привет</p>" + a)
+// }
+
+function but() {
+	var text = document.getElementById('name').value
+	alert('Вы ввели ' + text); 
+}
+
+Number(text)
+
+var a = 5;
+var b = 6;
+document.write(a + b)
+
+
+
+
+
