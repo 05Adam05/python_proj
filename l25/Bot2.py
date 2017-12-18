460c903b7f65223d310d7c502b3979c161b8b594
05Adam05 <32506935+05Adam05@users.noreply.github.com>
2017-12-18 11:29:42 +0300

    Add files via upload

--- l25/Bot2.py
+++ l25/Bot2.py
@@ -1,20 +1,36 @@
 import telebot
 
-TOKEN = "412526464:AAGPBU6liOdo8CsMmWJssFR08KPPZpJZaWg"
+TOKEN = "181530422:AAGTPK6IUBqWraxxKyf8a-PzjSCSCfodUWw"
 
 bot = telebot.TeleBot(TOKEN)
 
+@bot.message_handler(regexp="Привет")
+def answer(message):
+	bot.send_message(message.chat.id, "Валейкумассалам")
 
-@bot.message_handler(regexp="Адам")
-def cheker(message):
-	bot.send_message(message.chat.id, "STFU")
+# @bot.message_handler(func=lambda message:True)
+# def say_smth(message):
+# 	if:
+# 		pass
+# 		bot.send_message(message.chat.id, "Не понимаю")
+# 	elif message.text == "Саламуалейкум":
+# 		bot.reply_to(message, "Оооооо, братуха")
 
+# @bot.message_handler(regexp="Адам")
+# def cheker(message):
+# 	bot.send_message(message.chat.id, "STFU")
+
+
+
+@bot.message_handler(regexp="Новости")
+def answer(message):
+	bot.send_message(message.chat.id, "Все новсти в Махачкале вы можете узнать сдесь https://news.yandex.ru/Makhachkala")
 @bot.message_handler(commands=["start"])
 def start_kb(message):
-	our_kb = telebot.types.ReplyKeyboardMarkup()
-	our_kb.row("Адам","-")
-	our_kb.row("Привет","Здрасте","Пока")
-	bot.send_message(message.chat.id, "Введи оператор", reply_markup=our_kb)
+	our_kb2 = telebot.types.ReplyKeyboardMarkup()
+	our_kb2.row("События")
+	our_kb2.row("Новости")
+	bot.send_message(message.chat.id, "Введи оператор", reply_markup=our_kb2)
 
 
 if __name__=="__main__":
