40d628c3d6257049bcfb5908054ec4fca2a9c7ec
05Adam05 <32506935+05Adam05@users.noreply.github.com>
2017-12-20 12:40:01 +0300

    Add files via upload

--- adbot.py
+++ adbot.py
@@ -1,10 +1,11 @@
 import telebot
 import datetime
-v_1.0
+
 
 TOKEN = "455886429:AAEZs-Yt1PCPx1tlYAc2WWpi_JxhacJ5dnM"
 
 bot = telebot.TeleBot(TOKEN)
+
 
 @bot.message_handler(regexp="Новости")
 def answer(message):
@@ -12,17 +13,78 @@ def answer(message):
 
 # @bot.message_handler(regexp="Событие")
 # def answer(message):
-# 	if datetime.date.today == 2017-12-18:
-# 		bot.send_message(message.chat.id, "Урааааааааааааа")
+# 	data = datetime.date.today()
+# 	bot.send_message(message.chat.id, data)
+# 	if str(data) == "2017-12-20":
+# 		bot.send_message(message.chat.id, "Ураа")
+# 	else:
+# 		bot.send_message(message.chat.id, "Событий нет")
+
+
+@bot.message_handler(regexp="Событие")
+def answer(message):
+	data = datetime.date.today()
+	bot.send_message(message.chat.id, data)
+	if str(data) == "2017-12-20":
+		bot.send_message(message.chat.id, "Ураа")
+	elif str(data) == "2017-12-22":
+		bot.send_message(message.chat.id, "Бортеневская битва — сражение, произошедшее 22 декабря 1317 года у села Бортенево, в котором тверской князь Михаил Ярославич разбил объединённое войско московского князя Юрия Даниловича и темника Золотой Орды Кавгадыя, вторгшееся в пределы Тверского княжества. В этом сражении тверичи взяли в плен жену князя Юрия Даниловича,\
+		 сестру ордынского хана Узбека Кончаку и брата князя Бориса.\n\
+		 Можете подробнее узнать сдесь https://ru.wikipedia.org/wiki/%D0%91%D0%BE%D1%80%D1%82%D0%B5%D0%BD%D0%B5%D0%B2%D1%81%D0%BA%D0%B0%D1%8F_%D0%B1%D0%B8%D1%82%D0%B2%D0%B0")
+	elif str(data) == "2017-12-23":
+		bot.send_message(message.chat.id, "Ежегодно 23 декабря в Египте отмечают годовщину вывода из страны войск Великобритании, Франции и Израиля в 1956 году. Этот день стал одним из национальных праздников и получил название Национального дня освобождения Порт-Саида (Port Said's National Day), которое напоминает о городе, в котором развернулись самые масштабные военные действия \n\
+			 Подробнее сдесь http://www.calend.ru/holidays/0/0/2358/")
+	elif str(data) == "2017-12-24":
+		bot.send_message(message.chat.id, "Днепровско-Карпатская операция – стратегическая наступательная операция \
+			советской армии в годы Великой Отечественной войны, \
+			проведенная в период с 24 декабря 1943 года по 17 апреля 1944 года \
+			против немецко-румынских войск с целью освобождения Правобережной Украины.\
+			 Развернувшись на фронте около 1400 км, операция явилась одной из крупнейших в ходе всей войны. \n\
+			 Подробнее можете узнать сдесь http://www.calend.ru/event/7386/")
+	elif str(data):
+		bot.send_message(message.chat.id, "Пока данных о событиях нет")
+
+
+
+@bot.message_handler(regexp="Погода")
+def answer(message):
+	data = datetime.date.today()
+	bot.send_message(message.chat.id, data)
+	if str(data) == "2017-12-20":
+		bot.send_message(message.chat.id, "Днем +9°с \n\
+Ночью +6°с\n\
+Пасмурно")
+	elif str(data) == '2017-12-21':
+		bot.send_message(message.chat.id, "Днем +8°с\n\
+Ночью +4°с\n\
+Небольшой дождь")
+	elif str(data) == '2017-12-22':
+		bot.send_message(message.chat.id, "Днем +6°с\n\
+Ночью +2°с\n\
+Пасмурно")
+	elif str(data) == "2017-12-23":
+		bot.send_message(message.chat.id, "Днем +4°с\n\
+Ночью +4°с\n\
+Небльшой дождь")
+	elif str(data) == "2017-12-24":
+		bot.send_message(message.chat.id, "Днем +4°с\n\
+Ночью 0°с\n\
+Пасмурно")
+	elif str(data):
+		bot.send_message(message.chat.id, "Данных о погоде нет ")
+
+
+
+
 
 @bot.message_handler(commands=["start"])
 def start_kb(message):
 	b_kb = telebot.types.ReplyKeyboardMarkup()
-	b_kb.row("Новости")
+	b_kb.row("/start")
 	b_kb.row("Событие")
+	b_kb.row("Новости")
+	b_kb.row("Погода")
 	bot.send_message(message.chat.id, "Введи оператор", reply_markup=b_kb)
-	bot.send_message(message.chat.id, "Первое слово пиши с большой буквы")
-	bot.send_message(message.chat.id, datetime.date.today())
 
 @bot.message_handler(regexp="Адам")
 def cheker(message):
@@ -44,6 +106,10 @@ def answer(message):
 def answer(message):
 	bot.send_message(message.chat.id, "Салам Алейкум")
 
+@bot.message_handler(regexp="Салам Алейкум")
+def answer(message):
+	bot.send_message(message.chat.id, "Ва Алейкум Ассалам варахматуллахи")
+
 @bot.message_handler(regexp="Салют")
 def answer(message):
 	bot.send_message(message.chat.id, "Салам Алейкум")
@@ -58,7 +124,7 @@ def answer(message):
 
 @bot.message_handler(regexp="Как дела")
 def answer(message):
-	bot.send_message(message.chat.id, "Отлично, а утебя")
+	bot.send_message(message.chat.id, "Отлично, а у тебя")
 
 @bot.message_handler(regexp="Отлично")
 def answer(message):
@@ -66,7 +132,7 @@ def answer(message):
 
 @bot.message_handler(regexp="Хорошо")
 def answer(message):
-	bot.send_message(message.chat.id, "Это хорошо что у тебя хорошо")
+	bot.send_message(message.chat.id, "Это хорошо что хорошо")
 
 @bot.message_handler(regexp="Плохо")
 def answer(message):
@@ -80,21 +146,18 @@ def answer(message):
 def answer(message):
 	bot.send_message(message.chat.id, "Нормально значит")
 
-@bot.message_handler(regexp="Норм")
-def answer(message):
-	bot.send_message(message.chat.id, "Норм так нор")
-
 @bot.message_handler(regexp="Нормально")
 def answer(message):
 	bot.send_message(message.chat.id, "Красавчик!")
-
 
 @bot.message_handler(func=lambda message:True)
 def say_smth(message):
 	if message.text:
 		bot.send_message(message.chat.id, "Я пока это непонимаю")
-	elif message.text == "Салам Алейкум":
+	elif message.text == "Салам":
 		bot.reply_to(message, "Ва Алейкум Ассалам варахматуллахи")
+
+
 
 if __name__=="__main__":
 	bot.polling()  
